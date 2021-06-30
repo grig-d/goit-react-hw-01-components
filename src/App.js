1f4bf1cc449ae3edf12f3527bb020286f7fb7eaa
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-06-30 19:45:41 +0300

    statistics component done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
         stats={user.stats}
       />
 
-      <Statistics />
+      <Statistics title={'Upload stats'} stats={statisticalData} />
+      <Statistics stats={statisticalData} />
     </>
   );
 }
--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -1,12 +1,47 @@
 import styles from './Statistics.module.scss';
 import PropTypes from 'prop-types';
 
-const Statistics = () => <h1>Statistics</h1>;
+function getRandomColor() {
+  let charCollection = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += charCollection[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-// const setBg = () => {
-//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-//   document.body.style.backgroundColor = '#' + randomColor;
-//   color.innerHTML = '#' + randomColor;
-// };
+const Statistics = ({ title, stats }) => (
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>{title.toUpperCase()}</h2>}
+
+    <ul
+      className={styles.statList}
+    >
+      {stats.map(({ id, label, percentage }) => {
+        return (
+          <li
+            key={id}
+            className={styles.item}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Statistics;
